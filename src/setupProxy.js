6af5f8d8fc6bd119c1c817feff95dd191b288d09
Kareem Phillip-Jackson <kpjackson27@gmail.com>
2020-03-23 19:24:26 -0400

    check proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    proxy('/**', { target: 'https://c2t-cabq-open-data.s3.amazonaws.com' })
+    '/**',
+    createProxyMiddleware({
+      target: 'https://c2t-cabq-open-data.s3.amazonaws.com',
+      changeOrigin: true
+    })
   );
 };
